fe2d30fecf631e31176988b5b7c48a23725eaa54
Hawkins80 <hawkinsn@gilmour.org>
2017-03-10 10:54:27 -0500

    pigo servo, with new nav method

--- student.py
+++ student.py
@@ -217,7 +217,7 @@ class GoPiggy(pigo.Pigo):
     ### (kind of a big deal)
     ########################
 
-    def final(self):
+    def nav(self):
         print("-----------! NAVIGATION ACTIVATED !------------\n")
         print("[ Press CTRL + C to stop me, then run stop.py ]\n")
         print("-----------! NAVIGATION ACTIVATED !------------\n")
